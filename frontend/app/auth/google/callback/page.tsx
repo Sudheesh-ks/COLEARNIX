8c170757d6e818e295f63ce5d7069f9a5352c8f8
Sudheesh ks <sudheeshks132@gmail.com>
2026-04-24 17:18:40 +0530

    fix:updated auth page to support production

--- frontend/app/auth/google/callback/page.tsx
+++ frontend/app/auth/google/callback/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Loader from "@/app/components/Loader/Loader";
 import toast from "react-hot-toast";
 
-export default function GoogleCallbackPage() {
+function GoogleCallbackHandler() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -27,7 +27,13 @@ export default function GoogleCallbackPage() {
     }
   }, [router, searchParams]);
 
+  return <Loader />;
+}
+
+export default function GoogleCallbackPage() {
   return (
-    <Loader />
+    <Suspense fallback={<Loader />}>
+      <GoogleCallbackHandler />
+    </Suspense>
   );
 }
